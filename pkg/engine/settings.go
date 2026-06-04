f5c27a96340aab3483aa46d61db290ff16eb0be1
DazFather <39210887+DazFather@users.noreply.github.com>
2026-06-04 17:14:19 +0200

    Fix style/script links for local file

--- pkg/engine/settings.go
+++ pkg/engine/settings.go
@@ -74,7 +74,7 @@ func (s Settings) StyleURL(elem ...string) string {
 	if err != nil {
 		panic(err)
 	}
-	return "/" + link
+	return link
 }
 
 func (s Settings) ScriptURL(elem ...string) string {
@@ -85,7 +85,7 @@ func (s Settings) ScriptURL(elem ...string) string {
 	if err != nil {
 		panic(err)
 	}
-	return "/" + link
+	return link
 }
 
 func (s Settings) StyleTag(name string) string {
